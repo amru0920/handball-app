738d8fa356b8b858d693dbc16b47adc730adb564
amru0920 <muhdammar758@gmail.com>
2026-05-21 23:27:08 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HandballAnalysisSystem from './handball_v5'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HandballAnalysisSystem />
+  </React.StrictMode>
+)
